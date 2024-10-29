73bb37eab6a761e32f21e7cc70ebbcb1c28790d7
KHANHTRUONG <khanhtruong2904@gmail.com>
2024-10-29 15:17:49 +0700

    Add files via upload

---
+++ THUCHANH /TUẦN 5/baibuoi5.1.java
@@ -0,0 +1,11 @@
+public class baibuoi5 {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+
+        int max = (a > b) ? a : b;
+        System.out.println("Giá trị lớn nhất: " + max);
+        int min = (a < b) ? a : b;
+        System.out.println("Giá trị nhỏ nhất: " + min);
+    }
+}
